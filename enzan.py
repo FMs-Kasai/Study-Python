d972fdfd544a57b629a7ae7406376e4596d10ece
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-17 12:12:40 +0900

    演算子の基本

---
+++ enzan.py
@@ -0,0 +1,37 @@
+x = 10
+y = 2
+z = 10
+
+a = 8
+b = 3
+
+print(x + y) #和
+print(x - y) #差
+print(x * y) #積
+print(x / y) #商
+print(x % y) #余
+
+#等価
+print(x == z)
+print(x == y)
+print(x != y)
+
+#比較
+print(x > y)
+print(x < y)
+
+print(x >= y)
+print(x <= z)
+
+#or and
+print(a == 3 or b ==3)
+print(a==1 or b ==1)
+print(a == 8 and b == 3)
+
+#複合代入演算子
+x += 10
+z += y
+
+print(x)
+print(z)
+
