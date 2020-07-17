e7a7db113ddd1f56314601a41b8e35192f7e653c
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-17 11:45:09 +0900

    変数の基本

---
+++ list.py
@@ -0,0 +1,20 @@
+list01 = ["サリンジャー","オーウェル","スウィフト",""]
+
+print(list01)
+print(list01[0])
+print(list01[1])
+print(list01[2])
+
+list01[3] = "ディック"
+print(list01[3])
+
+# list01[4] = "kasai" 添え字エラー　配列のレンジを超えた挿入はできない
+# print(list01[4])
+
+#リストの中にリストを挿入できる
+list02 = [["kasai","sato"],["ubayama","sakuma"]]
+
+print(list02[0][0])
+print(list02[0][1])
+print(list02[1][0])
+print(list02[1][1])
