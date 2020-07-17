c7485870f5d3caf3b358327cf454c4461fee7604
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-17 11:52:45 +0900

    データ型の基本

---
+++ data-kata.py
@@ -0,0 +1,20 @@
+num01 = 999
+num02 = 1.23
+
+string_a = 'Hi!!'
+
+print(type(num01))
+print(type(num02))
+
+print(string_a)
+print(type(string_a))
+
+a = 10
+b = 1
+
+bool01 = (a==b)
+bool02 = (a>b)
+
+print(bool01)
+print(bool02)
+print(type(bool01))
