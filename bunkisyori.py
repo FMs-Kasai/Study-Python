b2f8bed509fced174f45a87b419ff3ef03455a76
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-17 12:29:55 +0900

    if文の基本

---
+++ bunkisyori.py
@@ -0,0 +1,19 @@
+age = 21
+
+if age >= 20:
+    print("adult")
+
+age = 18
+
+if age >= 20:
+    print("adult")
+else:
+    print("child")
+
+age = 0
+
+if age >= 20:
+    print("adult")
+elif age == 0:
+    print("baby")
+
