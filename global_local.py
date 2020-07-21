ccf9288fcaa2f68c73728b819956ae67d0c07a12
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-21 15:49:30 +0900

    global文について追加

--- global_local.py
+++ global_local.py
@@ -1,6 +1,8 @@
 def addition():
     a = 0  #addition関数内のローカル変数
     b = 1
+    global d #グローバル変数を定義することもできる
+    d = 7
     subtraction()
     print(a)
 
@@ -13,3 +15,5 @@ def subtraction():
 c = 5 #グローバル変数。関数内で参照可能
 addition()  #ローカル変数は名前が同じでも区別されるため　a = 0
 subtraction()
+d = 0
+print(d) #d=0が出力
