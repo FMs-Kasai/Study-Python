e294b72a3326ed12aa64914cd942a727227b597b
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-07-28 21:55:20 +0900

    strip関連のメソッドを使った処理の挙動確認

---
+++ remove_space.py
@@ -0,0 +1,8 @@
+message = "     Hello Word     "
+strip_message = message.strip()
+lstrip_message = message.lstrip()
+rstrip_message = message.rstrip()
+
+print(strip_message)
+print(lstrip_message)
+print(rstrip_message)
