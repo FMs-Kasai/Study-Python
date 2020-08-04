6b2fccad5c09711bd8eb765fb43d5292c2eb2a26
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-08-05 00:00:23 +0900

    カタカナの正規表現で長音を含む場合と含まない場合についての記述

--- regular_expression.py
+++ regular_expression.py
@@ -1,10 +1,18 @@
 import re
 
 word = "アークデーモン"
-re_katakana = re.compile(r'[\u30A1-\u30F4]+')
-status_kata = re_katakana.fullmatch(word)
+re_katakana_notyoon = re.compile(r"[\u30A1-\u30F4]+") #長音含まない
+status_kata = re_katakana_notyoon.fullmatch(word)
+
+re_katakana_oktyoon = re.compile(r"[\u30A1-\u30FC]+") #長音含む
+status_kata_tyoon = re_katakana_oktyoon.fullmatch(word)
 
 if status_kata:
+    print("ok")
+else:
+    print("no")
+
+if status_kata_tyoon:
     print("ok")
 else:
     print("no")
