aef1f1f26c3636f6ff28ff3c12b9700086650726
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-08-10 14:34:02 +0900

    正規表現についての学習

--- regular_expression.py
+++ regular_expression.py
@@ -1,5 +1,30 @@
 import re
+# .*は貪欲マッチ（マッチする文字列が複数あった場合一番長い文字列を選択する）
+# .?は非貪欲マッチ（上記の逆）
 
+def phonenumber_regex(phonenumber: str) -> bool:
+    pattern = re.compile(r"(\d\d\d-\d\d\d-\d\d\d\d)")
+
+    if pattern.fullmatch(phonenumber):
+        return True
+    else:
+        return False
+
+def password_check(password: str) -> bool:
+    pattern = re.compile(r"[a-zA-Z0-9]{8,20}")
+
+    if pattern.fullmatch(password):
+        return True
+    else:
+        return False
+
+print("電話番号を入力してください")
+phonenumber = input()
+print(phonenumber_regex(phonenumber))
+password = input()
+print(password_check(password))
+
+"""
 word = "アークデーモン"
 word2 = "ウドラー"
 
@@ -26,3 +51,4 @@ if status_teil_tyoon:
     print("ok-tyoon")
 else:
     print("no-tyoon")
+"""
