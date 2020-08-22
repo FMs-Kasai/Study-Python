e3fbc0167036a5e50c959e0ba7d96c414624471b
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-08-22 12:29:23 +0900

    都道府県クイズ

--- Quiz_kentyo.py
+++ Quiz_kentyo.py
@@ -17,11 +17,35 @@ def choice_prefecture(prefecture: list) -> str:
     prefecture_num = random.randint(0, 46)
 
     return prefecture[prefecture_num][0]
+
 # 正解か判定
+def judge_answer(answer: str) -> bool:
+    return None
+
+
 # ユーザの入力処理。
+def user_input_answer() -> str:
+    print("県庁所在地を入力してください")
+
+    answer = str(input())
+
+    if len(answer) <= 10:
+        return answer
+    else:
+        print("県庁所在地は10文字以内で入力してください")
+        return user_input_answer()
+
 # 結果をファイル出力
 # 出した都道府県覚えておく
 # 出した都道府県消す。
-prefecture = make_prefecture()
-print(choice_prefecture(prefecture))
+# 都道府県クイズ処理（メインプログラム）
+def prefecture_quiz() -> None:
+    prefecture = make_prefecture()
+    for question_count in range(1, 47):
+        question = choice_prefecture(prefecture)
+        print("問題{}:{}".format(question_count, question))
+        answer = user_input_answer()
+
+    return None
+prefecture_quiz()
 
